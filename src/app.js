2291dedaad612a10604707a07f15f63cb01dd4b4
aishah849 <aishahalrashid@gmail.com>
2022-05-09 02:05:00 +0800

    Added weather forecast with JS

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Saturday", "Sunday", "Monday", "Tuesday"];
@@ -44,6 +45,13 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "f617fc313f1abac3b9f67f0a1a41e6c4";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -69,6 +77,8 @@ function displayTemperature(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -100,7 +110,6 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
-displayForecast();
 
 document.querySelector("#search-form").addEventListener("submit", handleSubmit);
 
